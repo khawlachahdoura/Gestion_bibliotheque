8f419fd10f73f8dfd4f417a13103872b0b02098a
khawla <chahdoura20@gmail.com>
2021-03-09 19:31:54 +0100

    modification IDAOGérerLivre

--- gestion_bibliothèque/src/IserviceDAO/IDAOGérerLivre.java
+++ gestion_bibliothèque/src/IserviceDAO/IDAOGérerLivre.java
@@ -3,7 +3,10 @@ package IserviceDAO;
 import Entities.*;
 
 public interface IDAOGérerLivre {
-	void ajouter(Livre livre);
-	void modifier(Livre livre);
-	void supprimer(Livre livre);
+	boolean ajouter(Livre livre);
+	boolean updateISBN(int isbn, int aISBN);
+	boolean updateTitre(String titre, int aISBN);
+	boolean updateTheme(String theme, int aISBN);
+	boolean updateAuteur(String auteur, int aISBN);
+	boolean supprimer(int isbn );
 }
