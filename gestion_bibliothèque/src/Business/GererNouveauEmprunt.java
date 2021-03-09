cd4ac6eeeec75ed57432cc24fd0f74585d8edb5d
khawla <chahdoura20@gmail.com>
2021-03-09 22:25:59 +0100

    ajout de gererlivre et gerernouveauemp au package Business

--- gestion_bibliothèque/src/Business/GererNouveauEmprunt.java
+++ gestion_bibliothèque/src/Business/GererNouveauEmprunt.java
@@ -1,5 +1,43 @@
 package Business;
-
+import Entities.Emprunt;
+import Entities.Livre;
+import ImplementDAO.ImpDAOGererLivre;
+import ImplementDAO.ImpDAOGererNouveauEmp;
 public class GererNouveauEmprunt {
+
+	private ImpDAOGererNouveauEmp iDaoGererNouveauEmp;
+
+	public GererNouveauEmprunt(ImpDAOGererNouveauEmp _iDaoGererNouveauEmp) {
+
+		iDaoGererNouveauEmp = _iDaoGererNouveauEmp;
+
+	}
+
+	private boolean addEmprunt(Emprunt emprunt) {
+
+		if (iDaoGererNouveauEmp.ajouter(emprunt)) {
+			return true;
+		}
+		return false;
+
+	}
+
+	private boolean updateEmprunt(Emprunt emprunt) {
+
+		if (iDaoGererNouveauEmp.modifier(emprunt)) {
+			return true;
+		}
+		return false;
+
+	}
+
+	private boolean deleteEmprunt(Emprunt emprunt) {
+
+		if (iDaoGererNouveauEmp.supprimer(emprunt)) {
+			return true;
+		}
+		return false;
+
+	}
 
 }
